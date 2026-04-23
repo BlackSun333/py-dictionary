abf07f2e183134400e5c544a4468f111fe52bc09
Stanislav <staskobetc@gmail.com>
2026-04-23 09:38:54 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
+from typing import Any
+
+
 class Node:
-    def __init__(self, key, value, hash_value) -> None:
+    def __init__(self, key: Any, value: Any, hash_value: int) -> None:
         self.key = key
         self.value = value
         self.hash = hash_value
@@ -9,13 +12,13 @@ class Dictionary:
     def __init__(self, capacity: int = 8) -> None:
         self.capacity = capacity
         self.size = 0
-        self.table = [None] * self.capacity
+        self.table: list[Node | None] = [None] * self.capacity
         self.load_factor = 2 / 3
 
     def __len__(self) -> int:
         return self.size
 
-    def __setitem__(self, key, value) -> None:
+    def __setitem__(self, key: Any, value: Any) -> None:
         if self.size / self.capacity >= self.load_factor:
             self._resize()
 
@@ -23,22 +26,24 @@ class Dictionary:
         index = key_hash % self.capacity
 
         while self.table[index] is not None:
-            if self.table[index].key == key:
-                self.table[index].value = value
+            node = self.table[index]
+            if node and node.key == key:
+                node.value = value
                 return
             index = (index + 1) % self.capacity
 
         self.table[index] = Node(key, value, key_hash)
         self.size += 1
 
-    def __getitem__(self, key) -> None:
+    def __getitem__(self, key: Any) -> Any:
         key_hash = hash(key)
         index = key_hash % self.capacity
         start_index = index
 
         while self.table[index] is not None:
-            if self.table[index].key == key:
-                return self.table[index].value
+            node = self.table[index]
+            if node and node.key == key:
+                return node.value
 
             index = (index + 1) % self.capacity
             if index == start_index:
@@ -56,6 +61,6 @@ class Dictionary:
             if node is not None:
                 self.__setitem__(node.key, node.value)
 
-    def __repr__(self) -> None:
+    def __repr__(self) -> str:
         items = [f"{n.key}: {n.value}" for n in self.table if n]
         return "{" + ", ".join(items) + "}"
